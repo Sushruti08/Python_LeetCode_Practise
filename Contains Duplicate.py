9528e2083c63a7a5fd4f8319b7dbeb556b7eb91a
Sushruti08 <47374965+Sushruti08@users.noreply.github.com>
2019-02-09 16:39:57 -0700

    Update Contains Duplicate.py

--- Contains Duplicate.py
+++ Contains Duplicate.py
@@ -1,4 +1,4 @@
-/* Given an array of integers, find if the array contains any duplicates.
+''' Given an array of integers, find if the array contains any duplicates.
 
 Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
 
@@ -13,9 +13,8 @@ Output: false
 Example 3:
 
 Input: [1,1,1,3,3,4,3,2,4,2]
-Output: true */ 
+Output: true '''
 
-Solution:
 
 
 class Solution:
